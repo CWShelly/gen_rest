5b363801ed8fd10f0b1c4ffd46d4a4e8882d4201
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air-2.local>
2016-06-30 00:41:18 -0700

    editing server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname+'/app'));
+app.use(express.static(__dirname + '/app'));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
 
 app.listen(3000, ()=>{
     console.log('up on 3000');
